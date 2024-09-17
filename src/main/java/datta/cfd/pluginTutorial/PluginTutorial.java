2657a3d568e3092701e1dbf11bf0103cf01bc0dc
datta <160187517+lucesdenabidad@users.noreply.github.com>
2024-09-16 23:30:03 -0300

    Comentarios

--- src/main/java/datta/cfd/pluginTutorial/PluginTutorial.java
+++ src/main/java/datta/cfd/pluginTutorial/PluginTutorial.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public final class PluginTutorial extends JavaPlugin implements Listener {
 
-    public static final List<Material> BLOQUES_BLOQUEADOS = new ArrayList<>(List.of(
+    public static final List<Material> BLOQUES_BLOQUEADOS = new ArrayList<>(List.of( // --> Estamos haciendo una lista publica estatica para obtenerla desde otra clase.
             Material.STONE,
             Material.COBBLESTONE,
             Material.DIRT
@@ -21,19 +21,22 @@ public final class PluginTutorial extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        
+
         // # Eventos
-        Bukkit.getPluginManager().registerEvents(new BlockBreakListener(), this);
+        Bukkit.getPluginManager().registerEvents(new BlockBreakListener(), this); // --> Estamos registrando lo eventos de romper y colocar bloques.
         Bukkit.getPluginManager().registerEvents(new BlockPlaceListener(), this);
 
         // # Comandos
-        getCommand("helloworld").setExecutor(new HelloWorldCommand());
+        getCommand("helloworld").setExecutor(new HelloWorldCommand()); // --> Estamos registrando el comando 'helloworld' --> ¡Revisa plugin.yml!
 
+        // # Log
         getLogger().info("El plugin a cargado con éxito.");
     }
 
     @Override
     public void onDisable() {
+
+        // # Log
         getLogger().info("El plugin fue desactivado con éxito.");
     }
 
--- src/main/java/datta/cfd/pluginTutorial/eventos/BlockPlaceListener.java
+++ src/main/java/datta/cfd/pluginTutorial/eventos/BlockPlaceListener.java
@@ -15,15 +15,16 @@ public class BlockPlaceListener implements Listener {
     @EventHandler
     public void colocarBloque(BlockPlaceEvent event) {
         Player player = event.getPlayer();
-        Block block = event.getBlock();
-        Material type = block.getType();
+        Block block = event.getBlock(); // --> Estamos obteniendo el bloque del evento
+        Material type = block.getType(); // --> Estamos obteniendo el material del bloque
 
         if (PluginTutorial.BLOQUES_BLOQUEADOS.contains(type)) {
-            player.sendMessage("Este bloque no se puede colocar porque " + type + " esta bloqueado.");
-            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, SoundCategory.MASTER, 1, 1);
-            player.sendTitle("¡Error!", "Este bloque esta bloqueado.", 5, 60, 5);
+            player.sendMessage("Este bloque no se puede colocar porque " + type + " esta bloqueado."); // --> Estamos enviendo un mensaje al jugador.
+            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, SoundCategory.MASTER, 1, 1); // --> Estamos reproduciendo un sonido al jugador.
+            player.sendTitle("¡Error!", "Este bloque esta bloqueado.", 5, 60, 5); // --> Estamos enviandole un titulo y subtitulo al jugador.
 
-            event.setCancelled(true);
+            event.setCancelled(true); // --> Estamos cancelando el evento, es decir no se rompe el bloque.
+
         }
     }
 }
